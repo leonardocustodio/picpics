e8d72f3a2155ec6103ebd83ff263ac02ee166100
Leonardo Custodio <leonardo@custodio.me>
2020-04-14 03:36:59 -0300

    new settings screen

--- lib/settings_screen.dart
+++ lib/settings_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:picPics/constants.dart';
+import 'package:outline_gradient_button/outline_gradient_button.dart';
 
 class SettingsScreen extends StatefulWidget {
   static const id = 'settings_Screen';
@@ -29,143 +30,238 @@ class _SettingsScreenState extends State<SettingsScreen> {
                 child: Image.asset('lib/images/backarrowgray.png'),
               ),
               Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      SizedBox(
-                        height: 30.0,
-                      ),
-                      Text(
-                        "Email",
-                        style: TextStyle(
-                          fontFamily: 'Lato',
-                          color: Color(0xff606566),
-                          fontSize: 12,
-                          fontWeight: FontWeight.w300,
-                          fontStyle: FontStyle.normal,
-                          letterSpacing: -0.4099999964237213,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                      child: Container(
+                        height: 60.0,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              "Desafios diários",
+                              style: TextStyle(
+                                fontFamily: 'Lato',
+                                color: Color(0xff606566),
+                                fontSize: 12,
+                                fontWeight: FontWeight.w300,
+                                fontStyle: FontStyle.normal,
+                                letterSpacing: -0.4099999964237213,
+                              ),
+                            ),
+                            CupertinoSwitch(
+                              value: true,
+                              activeColor: kSecondaryColor,
+                              onChanged: (value) {},
+                            ),
+                          ],
                         ),
                       ),
-                      TextField(),
-                      SizedBox(
-                        height: 27.0,
-                      ),
-                      Text(
-                        "Notificação",
-                        style: TextStyle(
-                          fontFamily: 'Lato',
-                          color: Color(0xff606566),
-                          fontSize: 12,
-                          fontWeight: FontWeight.w300,
-                          fontStyle: FontStyle.normal,
-                          letterSpacing: -0.4099999964237213,
+                    ),
+                    Divider(
+                      color: kLightGrayColor,
+                      thickness: 1.0,
+                    ),
+                    Container(
+                      height: 60.0,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              "Quantidade de fotos",
+                              style: TextStyle(
+                                fontFamily: 'Lato',
+                                color: Color(0xff606566),
+                                fontSize: 12,
+                                fontWeight: FontWeight.w300,
+                                fontStyle: FontStyle.normal,
+                                letterSpacing: -0.4099999964237213,
+                              ),
+                            ),
+                            Text(
+                              "15",
+                              style: TextStyle(
+                                fontFamily: 'Lato',
+                                color: Color(0xff606566),
+                                fontSize: 18,
+                                fontWeight: FontWeight.w400,
+                                fontStyle: FontStyle.normal,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                      SizedBox(
-                        height: 13.0,
+                    ),
+                    Divider(
+                      color: kLightGrayColor,
+                      thickness: 1.0,
+                    ),
+                    Container(
+                      height: 60.0,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              "Horário da notificação",
+                              style: TextStyle(
+                                fontFamily: 'Lato',
+                                color: Color(0xff606566),
+                                fontSize: 12,
+                                fontWeight: FontWeight.w300,
+                                fontStyle: FontStyle.normal,
+                                letterSpacing: -0.4099999964237213,
+                              ),
+                            ),
+                            Text(
+                              "21:00",
+                              style: TextStyle(
+                                fontFamily: 'Lato',
+                                color: Color(0xff606566),
+                                fontSize: 18,
+                                fontWeight: FontWeight.w400,
+                                fontStyle: FontStyle.normal,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    ),
+                    Divider(
+                      color: kLightGrayColor,
+                      thickness: 1.0,
+                    ),
+                    Spacer(
+//                      flex: 2,
+                        ),
+                    Center(
+                      child: Column(
                         children: <Widget>[
-                          Text(
-                            "Desafios diários",
-                            style: TextStyle(
-                              fontFamily: 'Lato',
-                              color: Color(0xff606566),
-                              fontSize: 18,
-                              fontWeight: FontWeight.w400,
-                              fontStyle: FontStyle.normal,
+                          CupertinoButton(
+                            onPressed: () {
+                              print('test');
+                            },
+                            child: Container(
+                              width: 166.0,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Image.asset('lib/images/sharewithfriends.png'),
+                                  SizedBox(
+                                    width: 15.0,
+                                  ),
+                                  Text(
+                                    "Share with friends",
+                                    style: TextStyle(
+                                      fontFamily: 'Lato',
+                                      color: Color(0xff979a9b),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700,
+                                      fontStyle: FontStyle.normal,
+                                      letterSpacing: -0.4099999964237213,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
                           ),
-                          CupertinoSwitch(
-                            value: true,
-                            activeColor: kSecondaryColor,
-                            onChanged: (value) {},
+                          CupertinoButton(
+                            onPressed: () {
+                              print('test');
+                            },
+                            child: Container(
+                              width: 166.0,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: <Widget>[
+                                  Image.asset('lib/images/starrateapp.png'),
+                                  SizedBox(
+                                    width: 15.0,
+                                  ),
+                                  Text(
+                                    "Rate this app",
+                                    style: TextStyle(
+                                      fontFamily: 'Lato',
+                                      color: Color(0xff979a9b),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700,
+                                      fontStyle: FontStyle.normal,
+                                      letterSpacing: -0.4099999964237213,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
                           ),
                         ],
                       ),
-                      SizedBox(
-                        height: 22.0,
-                      ),
-                      Text(
-                        "Quantidade de fotos",
-                        style: TextStyle(
-                          fontFamily: 'Lato',
-                          color: Color(0xff606566),
-                          fontSize: 12,
-                          fontWeight: FontWeight.w300,
-                          fontStyle: FontStyle.normal,
-                          letterSpacing: -0.4099999964237213,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 18.0,
-                      ),
-                      Text(
-                        "15",
-                        style: TextStyle(
-                          fontFamily: 'Lato',
-                          color: Color(0xff606566),
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.normal,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 27.0,
-                      ),
-                      Text(
-                        "Horário do dia",
-                        style: TextStyle(
-                          fontFamily: 'Lato',
-                          color: Color(0xff606566),
-                          fontSize: 12,
-                          fontWeight: FontWeight.w300,
-                          fontStyle: FontStyle.normal,
-                          letterSpacing: -0.4099999964237213,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 18.0,
-                      ),
-                      Text(
-                        "21:00",
-                        style: TextStyle(
-                          fontFamily: 'Lato',
-                          color: Color(0xff606566),
-                          fontSize: 18,
-                          fontWeight: FontWeight.w400,
-                          fontStyle: FontStyle.normal,
-                        ),
-                      ),
-                      Spacer(),
-                      Container(
-                        height: 44.0,
-                        decoration: BoxDecoration(
-                          gradient: kPrimaryGradient,
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                        child: Center(
-                          child: Text(
-                            "Exportar biblioteca",
-                            style: TextStyle(
-                              fontFamily: 'Lato',
-                              color: kWhiteColor,
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700,
-                              fontStyle: FontStyle.normal,
-                              letterSpacing: -0.4099999964237213,
-                            ),
+                    ),
+                    SizedBox(
+                      height: 40.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                      child: CupertinoButton(
+                        onPressed: () {
+                          print('test');
+                        },
+                        padding: const EdgeInsets.all(0),
+                        child: OutlineGradientButton(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Image.asset('lib/images/logopremium.png'),
+                              SizedBox(
+                                width: 16.0,
+                              ),
+                              Text(
+                                "Get Premium now!",
+                                style: TextStyle(
+                                  fontFamily: 'Lato',
+                                  color: kSecondaryColor,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w700,
+                                  fontStyle: FontStyle.normal,
+                                  letterSpacing: -0.4099999964237213,
+                                ),
+                              ),
+                            ],
                           ),
+                          gradient: LinearGradient(colors: [Color(0xFFFFA4D1), Color(0xFFFFCC00)]),
+                          strokeWidth: 2.0,
+                          radius: Radius.circular(8.0),
                         ),
                       ),
-                      SizedBox(
-                        height: 10.0,
-                      ),
-                    ],
-                  ),
+                    ),
+//                    Spacer(
+//                      flex: 1,
+//                    ),
+//                    Container(
+//                      height: 44.0,
+//                      decoration: BoxDecoration(
+//                        gradient: kPrimaryGradient,
+//                        borderRadius: BorderRadius.circular(8.0),
+//                      ),
+//                      child: Center(
+//                        child: Text(
+//                          "Exportar biblioteca",
+//                          style: TextStyle(
+//                            fontFamily: 'Lato',
+//                            color: kWhiteColor,
+//                            fontSize: 16,
+//                            fontWeight: FontWeight.w700,
+//                            fontStyle: FontStyle.normal,
+//                            letterSpacing: -0.4099999964237213,
+//                          ),
+//                        ),
+//                      ),
+//                    ),
+                  ],
                 ),
               ),
             ],
