9e9a302d9ec95af2305bf89268edd8f73f2d26b2
Leonardo Custodio <leonardo@custodio.me>
2020-09-10 21:56:48 -0300

    changes

---
+++ lib/components/colorful_background.dart
@@ -0,0 +1,57 @@
+import 'dart:math';
+import 'package:picPics/constants.dart';
+import 'package:flutter/material.dart';
+
+class ColorfulBackground extends CustomPainter {
+  final Point moveBy;
+  ColorfulBackground({@required this.moveBy});
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint primaryGradient = Paint()..shader = kPrimaryGradient.createShader(Rect.fromLTWH(0, 0, size.width, size.height));
+    Paint secondaryGradient = Paint()..shader = kSecondaryGradient.createShader(Rect.fromLTWH(0, 0, size.width, size.height));
+    Paint pinkGradient = Paint()..shader = kPinkGradient.createShader(Rect.fromLTWH(0, 0, size.width, size.height));
+
+    Paint yellowPaint = Paint()
+      ..color = kYellowColor
+      ..style = PaintingStyle.fill;
+
+    Path secondaryPath = getSecondaryPath(size.width, size.height, moveBy.x - 20, moveBy.y * 2);
+    Path pinkPath = getPinkPath(size.width, size.height, -(moveBy.x / 3), -(moveBy.y / 2 - 30));
+    Path yellowPath = getYellowPath(size.width, size.height, moveBy.x / 2, -moveBy.y);
+
+    canvas.drawPaint(primaryGradient);
+    canvas.drawPath(pinkPath, pinkGradient);
+    canvas.drawPath(secondaryPath, secondaryGradient);
+    canvas.drawPath(yellowPath, yellowPaint);
+  }
+
+  Path getPinkPath(double x, double y, double moveByX, double moveByY) {
+    return Path()
+      ..moveTo(x * 1, y * 0.35 + moveByY)
+      ..quadraticBezierTo(x * 0.5 + moveByX, y * 0.4 + moveByY, 0 + moveByX, y * 0.3 + moveByY)
+      ..lineTo(0 * x + moveByX, 1 * y + moveByY)
+      ..lineTo(1 * x, y * 1 + moveByY)
+      ..close();
+  }
+
+  Path getYellowPath(double x, double y, double moveByX, double moveByY) {
+    return Path()
+      ..moveTo(x * 0.1 + moveByX, -20 + moveByY)
+      ..quadraticBezierTo(x * 0.2, y * 0.1, -x * 0.1, y * 0.2)
+      ..lineTo(0, 0)
+      ..close();
+  }
+
+  Path getSecondaryPath(double x, double y, double moveByX, double moveByY) {
+    return Path()
+      ..moveTo(0, y * 0.25 + moveByY / 3)
+      ..quadraticBezierTo(x * 0.75 + moveByX, y * 0.3 + moveByY, x, y * 0.5 - moveByY / 2)
+      ..lineTo(x * 1, y * 1)
+      ..lineTo(0, y * 1)
+      ..close();
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}
