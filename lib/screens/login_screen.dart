76ee31c1231274e1a89798f8cf44116a3126b4cf
Leonardo Custodio <leonardo@custodio.me>
2020-09-10 21:46:24 -0300

    added animation to login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,6 +9,8 @@ import 'package:picPics/stores/app_store.dart';
 import 'package:picPics/screens/tabs_screen.dart';
 import 'package:picPics/generated/l10n.dart';
 import 'package:provider/provider.dart';
+import 'package:simple_animations/simple_animations.dart';
+import 'package:supercharged/supercharged.dart';
 
 class LoginScreen extends StatefulWidget {
   static const id = 'login_screen';
@@ -17,12 +19,22 @@ class LoginScreen extends StatefulWidget {
   _LoginScreenState createState() => _LoginScreenState();
 }
 
-class _LoginScreenState extends State<LoginScreen> {
+class _LoginScreenState extends State<LoginScreen> with AnimationMixin {
   AppStore appStore;
+
+  AnimationController widthController;
+  AnimationController heightController;
+  Animation<double> moveByX;
+  Animation<double> moveByY;
 
   @override
   void initState() {
     super.initState();
+    widthController = createController()..mirror(duration: 3.seconds);
+    heightController = createController()..mirror(duration: 3.seconds);
+    moveByX = 0.0.tweenTo(30.0).animatedBy(widthController);
+    moveByY = 0.0.tweenTo(20.0).animatedBy(heightController);
+
     Analytics.sendCurrentScreen(Screen.login_screen);
   }
 
@@ -30,17 +42,18 @@ class _LoginScreenState extends State<LoginScreen> {
   void didChangeDependencies() {
     super.didChangeDependencies();
     appStore = Provider.of<AppStore>(context);
+    appStore.createDefaultTags(context);
   }
 
   @override
   Widget build(BuildContext context) {
-    appStore.createDefaultTags(context);
-
     return Scaffold(
       body: AnnotatedRegion<SystemUiOverlayStyle>(
         value: SystemUiOverlayStyle.dark,
         child: CustomPaint(
-          painter: ColorfulBackground(),
+          painter: ColorfulBackground(
+            moveBy: Point(moveByX.value, moveByY.value),
+          ),
           child: Container(
             constraints: BoxConstraints.expand(),
             child: SafeArea(
@@ -106,11 +119,8 @@ class _LoginScreenState extends State<LoginScreen> {
 }
 
 class ColorfulBackground extends CustomPainter {
-//  final Color strokeColor;
-//  final PaintingStyle paintingStyle;
-//  final double strokeWidth;
-//
-//  ColorfulBackground({this.strokeColor = Colors.black, this.strokeWidth = 3, this.paintingStyle = PaintingStyle.stroke});
+  final Point moveBy;
+  ColorfulBackground({@required this.moveBy});
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -122,9 +132,9 @@ class ColorfulBackground extends CustomPainter {
       ..color = kYellowColor
       ..style = PaintingStyle.fill;
 
-    Path secondaryPath = getSecondaryPath(size.width, size.height);
-    Path pinkPath = getPinkPath(size.width, size.height);
-    Path yellowPath = getYellowPath(size.width, size.height);
+    Path secondaryPath = getSecondaryPath(size.width, size.height, moveBy.x - 20, moveBy.y * 2);
+    Path pinkPath = getPinkPath(size.width, size.height, -(moveBy.x / 3), (moveBy.y / 2 - 20));
+    Path yellowPath = getYellowPath(size.width, size.height, moveBy.x / 2, -moveBy.y);
 
     canvas.drawPaint(primaryGradient);
     canvas.drawPath(pinkPath, pinkGradient);
@@ -132,29 +142,29 @@ class ColorfulBackground extends CustomPainter {
     canvas.drawPath(yellowPath, yellowPaint);
   }
 
-  Path getPinkPath(double x, double y) {
+  Path getPinkPath(double x, double y, double moveByX, double moveByY) {
     return Path()
-      ..moveTo(x * 1, y * 0.35)
-      ..quadraticBezierTo(x * 0.5, y * 0.4, 0, y * 0.3)
-      ..lineTo(0 * x, 1 * y)
-      ..lineTo(1 * x, y * 1)
+      ..moveTo(x * 1, y * 0.35 + moveByY)
+      ..quadraticBezierTo(x * 0.5 + moveByX, y * 0.4 + moveByY, 0 + moveByX, y * 0.3 + moveByY)
+      ..lineTo(0 * x + moveByX, 1 * y + moveByY)
+      ..lineTo(1 * x, y * 1 + moveByY)
       ..close();
   }
 
-  Path getYellowPath(double x, double y) {
+  Path getYellowPath(double x, double y, double moveByX, double moveByY) {
     return Path()
-      ..moveTo(x * 0.1, -20)
+      ..moveTo(x * 0.1 + moveByX, -20 + moveByY)
       ..quadraticBezierTo(x * 0.2, y * 0.1, -x * 0.1, y * 0.2)
       ..lineTo(0, 0)
       ..close();
   }
 
-  Path getSecondaryPath(double x, double y) {
+  Path getSecondaryPath(double x, double y, double moveByX, double moveByY) {
     return Path()
-      ..moveTo(0, y * 0.25)
-      ..quadraticBezierTo(x * 0.75, y * 0.3, x, y * 0.5)
+      ..moveTo(0, y * 0.25 + moveByY / 3)
+      ..quadraticBezierTo(x * 0.75 + moveByX, y * 0.3 + moveByY, x, y * 0.5 - moveByY / 2)
       ..lineTo(x * 1, y * 1)
-      ..lineTo(x * 0, y * 1)
+      ..lineTo(0, y * 1)
       ..close();
   }
 
