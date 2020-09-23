f56f47ecbd67224af9cd2a4284e6cb0fb9866095
Leonardo Custodio <leonardo@custodio.me>
2020-09-23 05:57:34 -0300

    fixed secret switch

---
+++ lib/widgets/secret_switch.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:picPics/constants.dart';
+import 'package:simple_animations/simple_animations.dart';
+import 'package:supercharged/supercharged.dart';
+
+class SecretSwitch extends StatefulWidget {
+  final bool value;
+  final Function onChanged;
+
+  SecretSwitch({
+    @required this.value,
+    @required this.onChanged,
+  });
+
+  @override
+  _SecretSwitchState createState() => _SecretSwitchState();
+}
+
+class _SecretSwitchState extends State<SecretSwitch> with AnimationMixin {
+  CustomAnimationControl control = CustomAnimationControl.PLAY;
+  bool goingRight = true;
+  bool initRight;
+
+  @override
+  void initState() {
+    super.initState();
+    initRight = widget.value;
+  }
+
+  void toggleDirection() {
+    setState(() {
+      goingRight = !goingRight;
+      control = (control == CustomAnimationControl.PLAY) ? CustomAnimationControl.PLAY_REVERSE : CustomAnimationControl.PLAY;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        CupertinoSwitch(
+          value: widget.value, // Provider.of<DatabaseManager>(context).userSettings.dailyChallenges,
+          activeColor: kYellowColor,
+          onChanged: (value) {
+            toggleDirection();
+            widget.onChanged(value);
+          },
+        ),
+        CustomAnimation<double>(
+          control: control,
+          tween: (-11.0).tweenTo(11.0),
+          duration: 200.milliseconds,
+          curve: goingRight ? Curves.easeOutCubic : Curves.easeInCubic,
+          builder: (context, child, value) {
+            return Positioned(
+              right: 25 + (initRight ? -value : value),
+              top: 12,
+              child: Image.asset('lib/images/lockcupertinogray.png'),
+            );
+          },
+        ),
+      ],
+    );
+  }
+}
