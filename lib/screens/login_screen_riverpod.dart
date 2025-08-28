f56202b7abe3abae80cfeae10c8bc62b82fb0aee
Leonardo Custodio <leonardo@custodio.me>
2025-08-28 12:57:32 -0300

    🔖 Checkpoint: Edit /Users/custodio/Development/Snowpine/picpics/lib/screens/login_screen_riverpod.dart

--- lib/screens/login_screen_riverpod.dart
+++ lib/screens/login_screen_riverpod.dart
@@ -131,7 +131,7 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
                       ),
                       _buildBottomButtons(),
                     ],
-                  );
+                  ));
               }),
             )),
           ],
