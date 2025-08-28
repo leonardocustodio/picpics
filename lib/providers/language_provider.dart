4305e4598a39307af37c88660c9c2a90b501be42
Leonardo Custodio <leonardo@custodio.me>
2025-08-28 12:38:42 -0300

    🔖 Checkpoint: Edit /Users/custodio/Development/Snowpine/picpics/lib/providers/language_provider.dart

--- lib/providers/language_provider.dart
+++ lib/providers/language_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod/riverpod.dart';
 import 'package:picpics/generated/l10n.dart' as language;
 
 class LanguageState {
