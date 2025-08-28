76d509b4dc092cd9b64e145efbc2e32999bb01d2
Leonardo Custodio <leonardo@custodio.me>
2025-08-28 12:39:21 -0300

    🔖 Checkpoint: Edit /Users/custodio/Development/Snowpine/picpics/lib/providers/login_provider.dart

--- lib/providers/login_provider.dart
+++ lib/providers/login_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:riverpod/riverpod.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:picpics/database/app_database.dart';
 import 'package:picpics/managers/analytics_manager.dart';
